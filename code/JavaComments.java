47221cd2fa6087a5868bda00fb81debb090d5d88
SoloLee <lee170@live.com>
2020-05-17 12:26:31 +0800

    Update JavaComments.java
    修改注释说明代码风格

--- code/JavaComments.java
+++ code/JavaComments.java
@@ -2,9 +2,9 @@ public class JavaComments {
 	public static void main(String[] args) {
 		// 单行注释:注释内容从//开始到本行结束 (常用)
 		/*
-			多行注释: 
-				用注释界定符将一段比较长的注释括起来 
-			*/
+		   多行注释: 
+			用注释界定符将一段比较长的注释括起来 
+		*/
 		/**
 		 * 文档注释:
 		 * 可以用来自动生成文档
